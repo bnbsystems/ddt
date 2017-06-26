039744bce7ce54d8926e167c75b865315a39f42f
arekbee <arekbee@gmail.com>
2017-06-26 12:28:13 +0200

    Update ddtRunSingle.R

--- R/ddtRunSingle.R
+++ R/ddtRunSingle.R
@@ -1,2 +1,29 @@
-ddtRunSingle <- function(ddtDir, preload, compare, expectedFileName) {    oldwd <- getwd()          setwd(file.path(ddtDir,"in"))    e1 <- new.env(parent = baseenv())     assign("res", preload(), envir = e1)    setwd(oldwd)        cat(serializeJSON(e1$res), file=file.path(ddtDir,"actual",expectedFileName), append = F)        expectedFilePath <- file.path(ddtDir, "expected",expectedFileName)    if(!file.exists(expectedFilePath)) {      warning("No expected file in ", expectedFilePath, ". It will create from actual file located under actual folder")      cat(serializeJSON(e1$res, pretty=T), file=expectedFilePath, append = F)    }           expected <- unserializeJSON(readLines(file(expectedFilePath, "r")) )    compare(expected, e1$res )
+
+ddtRunSingle <- function(ddtDir, preload, compare, expectedFileName, readSaveObj = NULL) {
+    if (is.null(readSaveObj)) {
+        readSaveObj <- getReadSaveObj.Default()
+    }
+
+    oldwd <- getwd()
+
+    ddtDirIn <- file.path(ddtDir, "in")
+    if (dir.exists(ddtDirIn)) {
+        setwd(ddtDirIn)
+    }
+    
+    e1 <- new.env(parent = baseenv()) 
+    assign("res", preload(), envir = e1)
+    setwd(oldwd)
+
+    readSaveObj$save(e1$res, filePath  = file.path(ddtDir, "actual", expectedFileName))
+
+    expectedFilePath <- file.path(ddtDir, "expected", expectedFileName)
+
+    if(!file.exists(expectedFilePath)) {
+        warning("No expected file in ", expectedFilePath, ".\n It will create from actual file located under actual folder.")
+        readSaveObj$save(e1$res, expectedFilePath)
+    }
+
+    expected <- readSaveObj$read(expectedFilePath)
+    compare(expected, e1$res )
 }
