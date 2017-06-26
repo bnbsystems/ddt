5738837494f32b88367533d3e479468355e7d479
arekbee <arekbee@gmail.com>
2017-06-26 12:35:18 +0200

    Create ddtCreateDirs.R

---
+++ R/ddtCreateDirs.R
@@ -0,0 +1,17 @@
+ddtCreateDirs <- function(ddtPath, testCaseName ="testCase1") {
+folderName <- c("actual", "expected", "in")
+
+    if (!base::dir.exists(ddtPath)) {
+        base::dir.create(ddtPath)
+    }
+
+    if (!base::dir.exists(base::file.path(ddtPath, testCaseName))) {
+        base::dir.create(base::file.path(ddtPath, testCaseName))
+    }
+    for (f in folderName) {
+        testCaseDir <- base::file.path(ddtPath, testCaseName, f)
+        if (!base::dir.exists(testCaseDir)) {
+            base::dir.create(testCaseDir)
+        }
+    }
+}
