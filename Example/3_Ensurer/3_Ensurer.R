b95c71e5d5c19bf1ea5342aa9907590d4c025351
arekbee <arekbee@gmail.com>
2017-06-26 12:33:29 +0200

    Update 3_Ensurer.R

--- Example/3_Ensurer/3_Ensurer.R
+++ Example/3_Ensurer/3_Ensurer.R
@@ -1,6 +1,10 @@
 #example ensurer 
 library(ensurer)
 preload <- function(){  myDf <- read.csv("example_input.csv")  return(lm(y~x, data=myDf))  }
-compare <- function(expected, actual){  actual$coefficients %>%  ensure_that(expected$coefficients, tolerance = 0.001,                                         msg = paste("wrong coefficents. Expected ", print(expected$coefficients) , " but is ",  print(actual$coefficients)))}
+compare <- function(expected, actual){
+  actual$coefficients %>%  ensure_that(expected$coefficients, tolerance = 0.001, 
+                                        msg = paste("wrong coefficents. Expected ", print(expected$coefficients) , " but is ",  print(actual$coefficients)))
+}
 
-run(ddtsDir, preload, compare)  
+ddtCreateDirs(ddtsDir)
+ddtRun(ddtsDir, preload, compare)
