1fb4428fad585d55c1573448f544dfc72d45ec24
arekbee <arekbee@gmail.com>
2017-06-26 12:32:14 +0200

    Update 2_RUnit.R

--- Example/2_RUnit/2_RUnit.R
+++ Example/2_RUnit/2_RUnit.R
@@ -1,5 +1,9 @@
 library(magrittr)
 library(RUnit)
 preload <- function(){  myDf <- read.csv("example_input.csv")  return(lm(y~x, data=myDf))  }
-compare <- function(expected, actual){  actual$coefficients %>%  checkEquals( expected$coefficients, tolerance = 0.001,                                         msg = paste("wrong coefficents. Expected ", print(expected$coefficients) , " but is ",  print(actual$coefficients)))}
-run(ddtsDir, preload, compare) 
+compare <- function(expected, actual){
+  actual$coefficients %>%  checkEquals( expected$coefficients, tolerance = 0.001, 
+                                        msg = paste("wrong coefficents. Expected ", print(expected$coefficients) , " but is ",  print(actual$coefficients)))
+}
+
+ddtRun(ddtsDir, preload, compare)
