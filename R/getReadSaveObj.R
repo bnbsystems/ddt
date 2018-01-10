46ce24fa87f1fce1a32f70cb5749dd9a8f52dc0a
arekbee <arekbee@MacBook-Pro-arekbee.local>
2018-01-10 10:58:22 +0100

    adding prefix before invoking functions

--- R/getReadSaveObj.R
+++ R/getReadSaveObj.R
@@ -2,32 +2,32 @@
 
 getReadSaveObj.JSON <- function() {
     list(save = function(obj, filePath) {
-        serObj <- serializeJSON(obj, pretty = T)
+        serObj <- jsonlite::serializeJSON(obj, pretty = T)
         cat(serObj, file = filePath, append = F)
     }, read = function(filePath) {
         file <- file(filePath, "r")
         expectedLines <- readLines(file, warn = F)
         close(file)
         unlink(file)
-        unserializeJSON(expectedLines) %>% return
+        jsonlite::unserializeJSON(expectedLines) %>% return
     }) %>% return
 }
 
 getReadSaveObj.RDS <- function() {
     list(save = function(obj, filePath) {
         print("saving to ", filePath)
-        save(obj, file = filePath)
+        base::save(obj, file = filePath)
     }, read = function(filePath) {
         print("Reading file ", filePath)
-        load(filePath) %>% return
+        base::load(filePath) %>% return
     }) %>% return
 }
 
 getReadSaveObj.CSV <- function() {
     list(save = function(obj, filePath) {
-        write.table(x = obj, file = filePath, append = FALSE, na = "NA", row.names = FALSE, col.names = TRUE)
+        utils::write.table(x = obj, file = filePath, append = FALSE, na = "NA", row.names = FALSE, col.names = TRUE)
     }, read = function(filePath) {
-        read.table(file = filePath, header = T, stringsAsFactors = TRUE) %>% return
+        utils::read.table(file = filePath, header = T, stringsAsFactors = TRUE) %>% return
     }) %>% return
 }
 
